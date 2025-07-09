ff18091ba3a301899ca60f548ac70688a7b3018c
mrshahstarbatch <mrshahstarbatch@gmail.com>
2025-07-09 12:06:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://mrshahstarbatch.github.io'
+}));
+
 app.use(express.json());
 
 // MongoDB connection
