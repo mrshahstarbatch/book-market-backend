c308d0f0cfcaa2adb12f61c99557c128c39e0162
mrshahstarbatch <mrshahstarbatch@gmail.com>
2025-07-11 00:38:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
   origin: 'https://mrshahstarbatch.github.io',
   methods: ['GET', 'POST'],
-  credentials: true
+  callowedHeaders: ['Content-Type'],
 }));
 
 app.use(express.json());
